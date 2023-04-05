4eb9e16ef0d4fa5256d92db0190ed49138cf6e68
Ryskhanryskhan <98904208+Ryskhanryskhan@users.noreply.github.com>
2023-04-05 21:23:48 +0600

    Add files via upload

---
+++ lab8/shop_back/api/migrations/0002_product_category.py
@@ -0,0 +1,17 @@
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('api', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='product',
+            name='category',
+            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='products', to='api.category'),
+        ),
+    ]
